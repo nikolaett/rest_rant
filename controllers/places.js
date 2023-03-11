0ce68484fa70f117dd136f461b3bc4d0e5a68b77
nikolaett <nikole.forbes92@gmail.com>
2023-03-11 17:41:22 -0500

    Comments can be added and seen on the MongoDB.

--- controllers/places.js
+++ controllers/places.js
@@ -58,7 +58,14 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect('/places/${req.params.id}')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 router.delete('/:id', (req, res) => {
@@ -69,11 +76,29 @@ router.get('/:id/edit', (req, res) => {
     res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
+router.delete('/:id/comment/:commentId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
 
