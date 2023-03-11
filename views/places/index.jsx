eeab61a39e6040cb1d1d080270faaadffbb89e5f
nikolaett <nikole.forbes92@gmail.com>
2023-03-11 14:36:55 -0500

    Formatting of cards.

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -8,16 +8,18 @@ import Card from 'react-bootstrap/Card'
 function index(data) {
     let placesFormatted = data.places.map((place) => {
         return(
-            <div className="col-sm-6">
-                <Card bg={'dark'} style={{width: '18rem'}} text={'light'} className="mb-3">
-                    <Card.Img variant="top" src={place.pic} alt={place.name}/>
-                        <Card.Body>
-                            <Card.Title><a href={`/places/${place.id}`}>{place.name}</a></Card.Title>
-                            <Card.Text className="mb-0">{place.cuisines}</Card.Text>
-                            <Card.Text>Located in {place.city}, {place.state}.</Card.Text>
-                        </Card.Body>
-                </Card>
-            </div>
+            <div className="row">
+                <div className="col-sm-6 col-md-4 col-lg-3" >
+                    <Card bg={'dark'} style={{width: '18rem'}} text={'light'} className="mb-3">
+                        <Card.Img variant="top" src={place.pic} alt={place.name}/>
+                            <Card.Body>
+                                <Card.Title><a href={`/places/${place.id}`}>{place.name}</a></Card.Title>
+                                <Card.Text className="mb-0">{place.cuisines}</Card.Text>
+                                <Card.Text>Located in {place.city}, {place.state}.</Card.Text>
+                            </Card.Body>
+                    </Card>
+                </div>
+            </div> 
         )
     })
     return(
